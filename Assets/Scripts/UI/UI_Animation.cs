cc58057ebe87beb08d148befc10705f06839d836
sasay7783 <sasay7783@nate.com>
2024-05-22 17:35:31 +0900

    [Add] UI Animation, Stack
    애니메이션과 Stack을 추가했습니다.
    
    그리고 아이콘은 조금 추가해뒀지만 더 추가할 예정

--- Assets/Scripts/UI/UI_Animation.cs
+++ Assets/Scripts/UI/UI_Animation.cs
@@ -1,20 +1,91 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Animation : MonoBehaviour
 {
-    public GameObject panel;
+    public CanvasGroup panelCanvasGroup;
+    public GameObject store;
+    public GameObject share;
+    public GameObject ad;
+    public GameObject setting;
+    public TMP_Text scoreTxt;
+
+    private Stack<GameObject> stack = new Stack<GameObject>();
 
     void Start()
     {
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButton(0))
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Ended)
-            {
-                panel.SetActive(false);
-            }
+            GameStart();
         }
+    }
+
+        public void GameStart()
+        {
+        //if (Input.touchCount > 0)
+        //{
+        //Touch touch = Input.GetTouch(0);
+        //if (touch.phase == TouchPhase.Ended)
+        //{
+
+        //panel.SetActive(false);
+        //}
+        //}
+
+        if(panelCanvasGroup != null)
+        {
+            panelCanvasGroup.DOFade(0, 1.0f).OnComplete(() =>
+            {
+                panelCanvasGroup.gameObject.SetActive(false);
+            });
+        }
+    }
+    private void OpenPopUp(GameObject go)
+    {
+        if(stack.Count > 0 )
+        {
+            GameObject current = stack.Peek();
+            ClosePopUp(current);
+        }
+        go.SetActive(true);
+        stack.Push(go);
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(go.transform.DOScale(1.3f, 0.1f));
+        sequence.Append(go.transform.DOScale(1.0f, 0.1f));
+        sequence.Play();
+    }
+
+    private void ClosePopUp(GameObject go)
+    {
+        if (stack.Count > 0 && stack.Peek() == go)
+        {
+            GameObject current = stack.Pop();
+
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(go.transform.DOScale(1.0f, 0.1f));
+            sequence.Append(go.transform.DOScale(0.3f, 0.1f));
+            sequence.OnComplete(() => go.SetActive(false));
+            sequence.Play();
+        }
+    }
+
+    public void StorePopUp()
+    {
+        OpenPopUp(store);
+    }
+
+    public void SettingPopUp()
+    {
+        OpenPopUp(setting);
+    }
+
+    public void ScoreUp()
+    {
+        // 도미노가 생성되서 놓여질 때마다 점수 상승
     }
 }
