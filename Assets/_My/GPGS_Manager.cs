307ea06406eaaad1f5a152b53518867dba7704d1
Jya0w0 <jya0w0@gmail.com>
2024-06-21 17:54:48 +0900

    [Add] Achievement

--- Assets/_My/GPGS_Manager.cs
+++ Assets/_My/GPGS_Manager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
 
 public class GPGSManager : MonoBehaviour
 {
@@ -26,5 +27,23 @@ public class GPGSManager : MonoBehaviour
             // to ask users to sign-in. Clicking it should call
             // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
         }
+    }
+
+    public void ShowAchievementUI()
+    {
+        // 전체 업적 표시.
+        PlayGamesPlatform.Instance.ShowAchievementsUI();
+    }
+
+    public void IncrementGPGSAchievement()
+    {
+        // 단계별 업적 증가.
+        // PlayGamesPlatform.Instance.IncrementAchievement(/*GPGSIds.단계별 업적 아이디, 성공 시 증가시킬 단계 값, (bool success) ==> { 성공 시 호출할 이벤트나 함수 }*/);
+    }
+
+    public void UnlockingGPGSAchievement()
+    {
+        // 업적 잠금 해제 및 공개.
+        //PlayGamesPlatform.Instance.UnlockAchievement(/*GPGSIds.단계별 잠금해제될 업적 아이디, (bool success) ==> { 성공 시 호출할 이벤트나 함수}*/);
     }
 }
