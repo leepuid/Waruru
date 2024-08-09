b915eaa00f56b92b9b8f86eb75d7378281752bd1
Jya0w0 <jya0w0@gmail.com>
2024-08-09 12:28:08 +0900

    [Add] Native Share
    SNS 공유 기능

--- Assets/Scripts/Util/CaptureShare.cs
+++ Assets/Scripts/Util/CaptureShare.cs
@@ -5,24 +5,24 @@ using System.IO;
 public class CaptureShare : MonoBehaviour
 {
 
-    //public void ClickShare()
-    //{
-    //    StartCoroutine(TakeScreenShot());
-    //}
+    public void ClickShare()
+    {
+        StartCoroutine(TakeScreenShot());
+    }
 
-    //private IEnumerator TakeScreenShot()
-    //{
-    //    yield return new WaitForEndOfFrame();
+    private IEnumerator TakeScreenShot()
+    {
+        yield return new WaitForEndOfFrame();
 
-    //    Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-    //    ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-    //    ss.Apply();
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
 
-    //    string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
-    //    File.WriteAllBytes(filePath, ss.EncodeToPNG());
+        string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
 
-    //    Destroy(ss);
+        Destroy(ss);
 
-    //    new NativeShare().AddFile(filePath).SetSubject("Share test").SetText("Test").Share();
-    //}
+        new NativeShare().AddFile(filePath).SetSubject("Waruru").SetText("¿Í¸£¸£!").Share();
+    }
 }
